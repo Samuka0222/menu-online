9f0f629f5acec7f6b2d91f03fab42360fe0f5475
Samuel Machado <mmachado0222@gmail.com>
2024-02-08 08:57:16 -0300

    feat: add initial home page full structure

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,6 +3,10 @@ import NavBar from "./_components/nav-bar";
 import { Phone } from "lucide-react";
 import SocialLinks from "@/app/_components/socials-links";
 import Services from "./_components/services";
+import Menu from "./_components/menu";
+import Reviews from "./_components/reviews";
+import Booking from "./_components/booking";
+import Footer from "./_components/footer";
 
 const HomePage = () => {
   return <>
@@ -12,7 +16,7 @@ const HomePage = () => {
       <section className="mt-12 flex flex-col justify-center items-center px-5">
         <h1 className="font-bold text-4xl text-center text-black">
           Escolha a sua comida <strong className="text-primary">Favorita</strong>.
-        </h1>
+        </h1>depoimento
 
         <p className="text-2xl text-center font-medium mt-5">
           Aproveite nosso cardápio! Escolha o que desejar e receber em sua casa de forma rápida e segura.
@@ -37,6 +41,10 @@ const HomePage = () => {
         </div>
 
         <Services />
+        <Menu />
+        <Reviews />
+        <Booking />
+        <Footer  />
       </section>
     </main>
   </>;
