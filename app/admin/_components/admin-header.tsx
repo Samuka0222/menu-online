bb2af3f61ef020d65e47927a9d579209e301269c
Samuel Machado <mmachado0222@gmail.com>
2024-03-11 17:04:55 -0300

    feat: add HomeButton and SideMenu components to AdminHeader.

--- app/admin/_components/admin-header.tsx
+++ app/admin/_components/admin-header.tsx
@@ -1,7 +1,9 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/app/_components/ui/avatar";
 import { Button } from "@/app/_components/ui/button";
 import Link from "next/link";
-import { Settings, UserRoundSearchIcon } from "lucide-react";
+import { UserRoundSearchIcon } from "lucide-react";
+import SideMenu from "@/app/_components/side-menu";
+import HomeButton from "@/app/_components/home-button";
 
 const AdminHeader = () => {
   return (
@@ -18,12 +20,9 @@ const AdminHeader = () => {
             <h1 className="text-lg font-bold">Menu On-Line</h1>
           </div>
         </div>
-        <div>
-          <Button variant='outline' size='icon' className="border-none shadow-default h-10 w-12 relative" asChild>
-            <Link href='/admin/settings'>
-              <Settings size={30} />
-            </Link>
-          </Button>
+        <div className="flex gap-3">
+          <HomeButton />
+          <SideMenu />
         </div>
       </div>
       <nav className="flex gap-2 justify-center items-center w-full">
@@ -40,6 +39,11 @@ const AdminHeader = () => {
         <Button variant='link' className="text-black text-base">
           <Link href='/admin/products'>
             Produtos
+          </Link>
+        </Button>
+        <Button variant='link' className="text-black text-base">
+          <Link href='/admin/settings'>
+            Restaurante
           </Link>
         </Button>
       </nav>
